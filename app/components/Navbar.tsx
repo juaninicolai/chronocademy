9807aca1b78c99d7bdeeb2fcf2ade1e282a3dc53
juaninicolai <juan.nicolai@maersk.com>
2024-08-05 20:42:32 +0200

    Add navbar structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,15 +3,20 @@ import Image from "next/image";
 
 export default function Navbar() {
     return <nav>
-        <h1>Chronocademy</h1>
-        <Link href={"/about"}>About us</Link>
-        <Link href={"/features"}>Features</Link>
-        <Link href={"/faq"}>FAQ</Link>
-        <Image
-            src="/logo.svg"
-            alt="chronocademy logo"
-            width={140}
-            height={76}
-        />
+        <div className={"flex justify-between w-full px-4"}>
+            <div>
+                <Image
+                    src="/logo.svg"
+                    alt="chronocademy logo"
+                    width={140}
+                    height={76}
+                />
+            </div>
+            <div className={"flex space-x-4"}>
+                <Link href={"#about"}>About us</Link>
+                <Link href={"#features"}>Features</Link>
+                <Link href={"#faq"}>FAQ</Link>
+            </div>
+        </div>
     </nav>;
 }
