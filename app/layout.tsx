2f64d527a784d1704a7bbec7f02f8029ed2b08b0
juaninicolai <juan.nicolai@maersk.com>
2024-08-15 15:35:28 +0200

    Add features section

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import React, { ReactNode } from "react";
+import ListItem from "@/app/components/ListItem";
 
 export const metadata: Metadata = {
     title: "Chronocademy",
@@ -18,6 +19,14 @@ export default function RootLayout({children}: Readonly<{ children: ReactNode; }
         <body>{children}
             <Navbar/>
             <Hero/>
+        <div>
+            <ul className={"flex justify-around py-1"}>
+                <ListItem src={"/skills_exchange.svg"} alt={"skills icon"} text={"Skill Exchange"} />
+                <ListItem src={"/google_meet.svg"} alt={"google meet icon"} text={"Google Meet Integration"} />
+                <ListItem src={"/chrono_wallet.svg"} alt={"chrono wallet icon"} text={"Chrono Wallet"} />
+                <ListItem src={"/flexible_payments.svg"} alt={"flexible payments icon"} text={"Flexible Payments"} />
+            </ul>
+        </div>
         </body>
         </html>
     );
