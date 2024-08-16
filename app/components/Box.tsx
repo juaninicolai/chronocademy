f36459f4fd5f4a21f09abde277841957f19fe5c3
juaninicolai <juan.nicolai@maersk.com>
2024-08-16 17:05:18 +0200

    Add Box reusable component

---
+++ app/components/Box.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import Image from "next/image";
+
+interface BoxProps {
+    color: string;
+    image: string;
+    title: string;
+    body: ReactNode;
+}
+
+const Box: React.FC<BoxProps> = ({ color, image, title, body }) => {
+    return (
+        <div className={`flex flex-col gap-3 p-12 px-11 flex-1 rounded-lg ${color}`}>
+            <div className={"flex items-center gap-3"}>
+                <Image src={image} alt={title} width={40} height={40}/>
+                <p id={"box-title"}>{title}</p>
+            </div>
+            <p id={"box-body"}>{body}</p>
+        </div>
+    );
+};
+
+export default Box;
