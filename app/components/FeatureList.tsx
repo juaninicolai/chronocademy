3b6f2d984381f61d4fdbd1c9154bda0aeabbb7cf
juaninicolai <juan.nicolai@maersk.com>
2024-08-19 20:56:48 +0200

    Format code

--- app/components/FeatureList.tsx
+++ app/components/FeatureList.tsx
@@ -5,10 +5,30 @@ import React, { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const features = [
-    { src: "/skills_exchange.svg", alt: "skills icon", text: "Skill Exchange", dialogText: "Earn and spend Chrono to foster a community-driven approach to education" },
-    { src: "/google_meet.svg", alt: "google meet icon", text: "Google Meet Integration", dialogText: "Scheduling and video conferencing through Google Calendar and Google Meet" },
-    { src: "/chrono_wallet.svg", alt: "chrono wallet icon", text: "Chrono Wallet", dialogText: "Track your earned and spent time credits easily." },
-    { src: "/flexible_payments.svg", alt: "flexible payments icon", text: "Flexible Payments", dialogText: "Convert Chrono to cash or purchase additional credits." },
+    {
+        src: "/skills_exchange.svg",
+        alt: "skills icon",
+        text: "Skill Exchange",
+        dialogText: "Earn and spend Chrono to foster a community-driven approach to education"
+    },
+    {
+        src: "/google_meet.svg",
+        alt: "google meet icon",
+        text: "Google Meet Integration",
+        dialogText: "Scheduling and video conferencing through Google Calendar and Google Meet"
+    },
+    {
+        src: "/chrono_wallet.svg",
+        alt: "chrono wallet icon",
+        text: "Chrono Wallet",
+        dialogText: "Track your earned and spent time credits easily."
+    },
+    {
+        src: "/flexible_payments.svg",
+        alt: "flexible payments icon",
+        text: "Flexible Payments",
+        dialogText: "Convert Chrono to cash or purchase additional credits."
+    },
 ];
 
 export default function FeatureList() {
