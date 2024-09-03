cb07e7915308436c395735763cc94934f7c20e59
juaninicolai <juan.nicolai@maersk.com>
2024-09-03 20:40:34 +0200

    Fix padding after modifying CTA

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <div id={"hero-section"}
              className={"flex justify-between bg-gradient-to-r from-primary-orange-200 to-primary-orange-400"}>
-            <div id={"hero-text"} className={"flex flex-col justify-center pl-28 space-y-8 py-20"}>
+            <div id={"hero-text"} className={"flex flex-col justify-center pl-28 space-y-8 pt-20 pb-19"}>
                 <h3 className={"font-roboto text-2xl"}>The platform where time becomes knowledge</h3>
                 <h1 className={"text-6xl font-bold"}>Transform the way <br/>you <span
                     className={"text-primary-blue-300"}>learn & teach</span></h1>
@@ -14,7 +14,7 @@ export default function Hero() {
                 .</p>
                 <CTA/>
             </div>
-            <div id={"hero-image"} className={"py-20"}>
+            <div id={"hero-image"} className={"py-20 pb-19"}>
                 <Image
                     src="/hero.svg"
                     alt="chronocademy illustration for hero section"
