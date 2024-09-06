cd9cff65a787add41eea30d47515a7a10d4ddda5
juaninicolai <juan.nicolai@maersk.com>
2024-09-06 14:32:50 +0200

    Add type

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,21 +1,17 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import 'dotenv/config';
 
-let app : FirebaseApp;
-try {
-    const firebaseConfig = {
-        apiKey: process.env.API_KEY,
-        authDomain: process.env.AUTH_DOMAIN,
-        projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
-        messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID
-    };
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
 
-    app = initializeApp(firebaseConfig);
-    console.log('Firebase initialized successfully');
-} catch (error) {
-    console.error('Error initializing Firebase:', error);
-}
+const app : FirebaseApp = initializeApp(firebaseConfig);
+
+console.log('Firebase initialized successfully');
 
 export { app };
