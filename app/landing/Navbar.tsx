a034d74c909650ed6883c8b227ac83280bd1ec88
juaninicolai <juan.nicolai@maersk.com>
2024-09-23 11:52:38 +0200

    Remove old code

--- app/landing/Navbar.tsx
+++ app/landing/Navbar.tsx
@@ -1,33 +1,3 @@
-// import Link from "next/link";
-// import Image from "next/image";
-//
-// export default function Navbar() {
-//   return (
-//     <nav>
-//       <div className={"flex justify-between px-28"}>
-//         <div>
-//           <Link href={"/"}>
-//             <Image
-//               src="/logo.svg"
-//               alt="chronocademy logo"
-//               className="h-[76px]"
-//               width={140}
-//               height={76}
-//             />
-//           </Link>
-//         </div>
-//         <div
-//           id={"nav-links"}
-//           className={"flex space-x-4 items-center text-primary-blue-500"}
-//         >
-//           <Link href={"#how-it-works"}>How it works</Link>
-//           <Link href={"#faqs"}>FAQ</Link>
-//           <Link href={"#about-us"}>About us</Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
 "use client";
 import Link from "next/link";
 import Image from "next/image";
@@ -51,7 +21,7 @@ export default function Navbar() {
                   src="/logo.svg"
                   alt="chronocademy logo"
                   className="h-[56px] sm:h-[76px]"
-                  width={120}  // Adjust width and height for responsiveness
+                  width={120}
                   height={56}
               />
             </Link>
