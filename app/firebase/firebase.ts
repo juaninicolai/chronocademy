93cc455ffe61a5aacbb59fe4fbaede83663904c8
juaninicolai <juan.nicolai@maersk.com>
2024-09-27 14:18:46 +0200

    Test without NEXT_PUBLIC

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,7 +1,7 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    apiKey: process.env.API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
