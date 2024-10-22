d449e6eb1d4e6b40f46d5279a7dbf70176edcd37
Veselin Ivanov <me@veselyn.com>
2024-10-22 18:11:10 +0200

    feat: separate main from RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import React, { ReactNode } from "react";
 import { Inter, Roboto } from "next/font/google";
-import Navbar from "@/app/landing/Navbar";
-import Footer from "@/app/landing/Footer";
 import classNames from "classnames";
 
 export const metadata: Metadata = {
@@ -34,7 +32,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={classNames("w-full", inter.variable, roboto.variable)}>
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
