5ab170e2e526c30e5f4491d16b99417d8c1cc517
juaninicolai <juan.nicolai@maersk.com>
2024-10-25 14:14:55 +0200

    feat: remove unused lib

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { SessionProvider } from "./session";
 import { AppSidebar } from "./sidebar";
