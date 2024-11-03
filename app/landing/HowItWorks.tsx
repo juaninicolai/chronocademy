2aa0caaef1cf41ea6a6c908a313b677897d70b15
OmarGPuertas <g.p.omar@hotmail.com>
2024-11-03 19:23:28 +0100

    changes in landing page

--- app/landing/HowItWorks.tsx
+++ app/landing/HowItWorks.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Box from "@/app/landing/Box";
+import Image from "next/image";
 
 export default function HowItWorks() {
   const [isMobile, setIsMobile] = useState(false);
@@ -15,8 +16,10 @@ export default function HowItWorks() {
       title: "Earn Chronos",
       body: (
         <>
-          Use some of your time to <span className="font-bold">teach</span> any of your skills to someone else or{" "}
-          <span className="font-bold">refer</span> us to a friend and earn Chronos for it.
+          Use some of your time to <span className="font-bold">teach</span> any
+          of your skills to someone else or{" "}
+          <span className="font-bold">refer</span> us to a friend and earn
+          Chronos for it.
         </>
       ),
     },
@@ -26,21 +29,26 @@ export default function HowItWorks() {
       title: "Spend Chronos",
       body: (
         <>
-          Use your Chronos to <span className="font-bold">learn</span> a skill from someone else or to{" "}
-          <span className="font-bold">unlock rewards or services</span> offered by Chronocademy.
+          Use your Chronos to <span className="font-bold">learn</span> a skill
+          from someone else or to{" "}
+          <span className="font-bold">unlock rewards or services</span> offered
+          by Chronocademy.
         </>
       ),
     },
     {
-      color: "bg-gradient-to-r from-secondary-yellow-100 to-secondary-yellow-400",
+      color:
+        "bg-gradient-to-r from-secondary-yellow-100 to-secondary-yellow-400",
       image: "/buy_chronos.svg",
       title: "Trade Chronos",
       body: (
         <>
-          If you do not want or do not have time to teach or learn a skill, you can{" "}
-          <span className="font-bold">buy or sell</span> Chronos on the platform.
+          If you do not want or do not have time to teach or learn a skill, you
+          can <span className="font-bold">buy or sell</span> Chronos on the
+          platform.
         </>
       ),
+      extraIcon: "/NotInBeta.png", // Specify the path to the 'Not in Beta' image only for this box
     },
   ];
 
@@ -67,12 +75,16 @@ export default function HowItWorks() {
   };
 
   return (
-    <div id="how-it-works" className="flex flex-col justify-center items-center sm:py-16">
+    <div
+      id="how-it-works"
+      className="flex flex-col justify-center items-center sm:py-16"
+    >
       <h1 className="font-inter sm:text-4xl text-secondary-black-500 font-extrabold">
         How It Works
       </h1>
       <h2 className="font-roboto text-h2 py-4 text-center">
-        Explore the various ways to use your Chronos and maximize your experience.
+        Explore the various ways to use your Chronos and maximize your
+        experience.
       </h2>
 
       {/* Mobile Carousel */}
@@ -90,13 +102,22 @@ export default function HowItWorks() {
           )}
 
           {/* Box */}
-          <div className="w-[85%] flex justify-center mx-auto"> {/* Setting width to 93% for mobile */}
+          <div className="w-[85%] flex justify-center mx-auto">
             <Box
               color={boxes[currentIndex].color}
               image={boxes[currentIndex].image}
               title={boxes[currentIndex].title}
               body={boxes[currentIndex].body}
             />
+            {/* Add "Not in Beta" badge if the current box is "Trade Chronos" */}
+            {boxes[currentIndex].title === "Trade Chronos" && (
+              <Image
+                src={boxes[currentIndex].extraIcon}
+                alt="Not in Beta"
+                className="absolute top-[+20px] right-[+10px] h-[25px] w-auto"
+                style={{ transform: "rotate(30deg)" }} // Rotate the badge by 20 degrees
+              />
+            )}
           </div>
 
           {/* Right Arrow */}
@@ -114,13 +135,22 @@ export default function HowItWorks() {
         /* Desktop View */
         <div className="flex flex-wrap justify-center gap-10 pt-8">
           {boxes.map((box, index) => (
-            <div key={index} className="w-full sm:w-[30%]"> {/* Full width for mobile, smaller for desktop */}
+            <div key={index} className="w-full sm:w-[30%] relative">
               <Box
                 color={box.color}
                 image={box.image}
                 title={box.title}
                 body={box.body}
               />
+              {/* Add "Not in Beta" badge if this box is "Trade Chronos" */}
+              {box.title === "Trade Chronos" && (
+                <Image
+                  src={box.extraIcon}
+                  alt="Not in Beta"
+                  className="absolute top-[+20px] right-[+20px] h-[25px] w-auto"
+                  style={{ transform: "rotate(30deg)" }}
+                />
+              )}
             </div>
           ))}
         </div>
