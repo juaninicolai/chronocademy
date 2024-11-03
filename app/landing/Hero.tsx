8ea00719f8fb78f973ce5d7f38ce33217768630f
OmarGPuertas <g.p.omar@hotmail.com>
2024-11-03 19:23:28 +0100

    changes in landing page

--- app/landing/Hero.tsx
+++ app/landing/Hero.tsx
@@ -12,16 +12,13 @@ export default function Hero() {
                 id="hero-text"
                 className="flex flex-col justify-center space-y-6 sm:space-y-8 px-6 sm:pl-28"
             >
-                <h3 className="font-roboto text-xl sm:text-2xl">
-                    The platform where time becomes knowledge
-                </h3>
                 <h1 className="text-4xl sm:text-6xl font-bold">
                     Transform the way <br />
-                    you <span className="text-primary-blue-300">learn & teach</span>
+                    you <span className="text-primary-blue-300">learn and teach</span>
                 </h1>
                 <p className="font-roboto text-lg sm:text-2xl">
-                    Earn time credits by teaching, spend them to learn. <br />
-                    No money needed.
+                Chronocademy - The Ultimate Skill Exchange Platform. <br />
+                    Earn time credits by teaching, spend them to learn. 
                 </p>
                 <CTA />
             </div>
@@ -30,7 +27,7 @@ export default function Hero() {
             <div id="hero-image" className="hidden sm:block py-20">
                 <Image
                     src="/hero.svg"
-                    alt="chronocademy illustration for hero section"
+                    alt="Online education platform"
                     className="h-[416px]"
                     width={490}
                     height={416}
