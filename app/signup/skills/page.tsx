8f6e50995ba6061c08f6ca474d1364be03d94d91
juaninicolai <juan.nicolai@maersk.com>
2024-11-23 12:11:26 +0100

    feat: use tabs in skills page

--- app/signup/skills/page.tsx
+++ app/signup/skills/page.tsx
@@ -23,6 +23,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { Textarea } from "@/components/ui/textarea";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const SkillsFormSchema = z.object({
   skills: z
@@ -82,107 +83,221 @@ export default function SkillsPage() {
 
   return (
     <Form {...form}>
-      <form className="space-y-4" onSubmit={form.handleSubmit(handleSubmit)}>
-        {/* TODO: Allow people to add more skills  */}
-        <div className={"space-y-2"}>
-          {skillsFieldArray.fields.map((item, index) => (
-            <fieldset className={"flex items-end gap-4"} key={item.id}>
-              <FormField
-                control={form.control}
-                name={`skills.${index}.category`}
-                render={({ field }) => (
-                  <FormItem className="space-y-0">
-                    <FormLabel className={"text-base"}>Category</FormLabel>
-                    <Select
-                      defaultValue={field.value}
-                      disabled={field.disabled}
-                      onValueChange={field.onChange}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-[280px]">
-                          <SelectValue placeholder="Category" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {/* TODO: Use categories from database */}
-                        <SelectItem value="languages">Languages</SelectItem>
-                        <SelectItem value="artsAndHumanities">
-                          Arts & Humanities
-                        </SelectItem>
-                        <SelectItem value="businessAndMarketing">
-                          Business & Marketing
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+      <div className={"space-y-4"}>
+        <h3>Please fill in the information about your skills.</h3>
+        <h3>
+          If you are only interested in either teaching or learning a skill,
+          just leave on section blank.
+        </h3>
+        <form className="space-y-4" onSubmit={form.handleSubmit(handleSubmit)}>
+          <Tabs defaultValue="teach" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="teach">Teach</TabsTrigger>
+              <TabsTrigger value="learn">Learn</TabsTrigger>
+            </TabsList>
+            <TabsContent value="teach">
+              {/* TODO: Allow people to add more skills  */}
+              <h5>What skills would you like to teach?</h5>
+              <div className={"space-y-2"}>
+                {skillsFieldArray.fields.map((item, index) => (
+                  <fieldset className={"flex items-end gap-4"} key={item.id}>
+                    <FormField
+                      control={form.control}
+                      name={`skills.${index}.category`}
+                      render={({ field }) => (
+                        <FormItem className="space-y-0">
+                          <FormLabel className={"text-base"}>
+                            Category
+                          </FormLabel>
+                          <Select
+                            defaultValue={field.value}
+                            disabled={field.disabled}
+                            onValueChange={field.onChange}
+                          >
+                            <FormControl>
+                              <SelectTrigger className="w-[280px]">
+                                <SelectValue placeholder="Category" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              {/* TODO: Use categories from database */}
+                              <SelectItem value="languages">
+                                Languages
+                              </SelectItem>
+                              <SelectItem value="artsAndHumanities">
+                                Arts & Humanities
+                              </SelectItem>
+                              <SelectItem value="businessAndMarketing">
+                                Business & Marketing
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
 
-              <FormField
-                control={form.control}
-                name={`skills.${index}.skill`}
-                render={({ field }) => (
-                  <FormItem className="space-y-0">
-                    <FormLabel className={"text-base"}>Skill</FormLabel>
-                    <Select
-                      defaultValue={field.value}
-                      disabled={field.disabled}
-                      onValueChange={field.onChange}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-[280px]">
-                          <SelectValue placeholder="Skill" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="english">English</SelectItem>
-                        <SelectItem value="history">History</SelectItem>
-                        <SelectItem value="economics">Economics</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {skillsFieldArray.fields.length > 1 && (
-                <Button
-                  size={"icon"}
-                  variant={"destructive"}
-                  onClick={() => handleRemoveSkill(index)}
-                >
-                  <CircleMinus />
+                    <FormField
+                      control={form.control}
+                      name={`skills.${index}.skill`}
+                      render={({ field }) => (
+                        <FormItem className="space-y-0">
+                          <FormLabel className={"text-base"}>Skill</FormLabel>
+                          <Select
+                            defaultValue={field.value}
+                            disabled={field.disabled}
+                            onValueChange={field.onChange}
+                          >
+                            <FormControl>
+                              <SelectTrigger className="w-[280px]">
+                                <SelectValue placeholder="Skill" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <SelectItem value="english">English</SelectItem>
+                              <SelectItem value="history">History</SelectItem>
+                              <SelectItem value="economics">
+                                Economics
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    {skillsFieldArray.fields.length > 1 && (
+                      <Button
+                        size={"icon"}
+                        variant={"destructive"}
+                        onClick={() => handleRemoveSkill(index)}
+                      >
+                        <CircleMinus />
+                      </Button>
+                    )}
+                  </fieldset>
+                ))}
+                <Button size={"sm"} type={"button"} onClick={handleAddSkill}>
+                  <CirclePlus />
+                  Add skill
                 </Button>
-              )}
-            </fieldset>
-          ))}
-          <Button size={"sm"} type={"button"} onClick={handleAddSkill}>
-            <CirclePlus />
-            Add skill
-          </Button>
-        </div>
-        <FormField
-          control={form.control}
-          name="profileDescription"
-          render={({ field }) => (
-            <FormItem className="space-y-0">
-              <FormLabel className={"text-base"}>Profile description</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+              </div>
+            </TabsContent>
+            <TabsContent value="learn">
+              {/* TODO: Allow people to add more skills  */}
+              <h5>What skills would you like to develop?</h5>
+              <div className={"space-y-2"}>
+                {skillsFieldArray.fields.map((item, index) => (
+                  <fieldset className={"flex items-end gap-4"} key={item.id}>
+                    <FormField
+                      control={form.control}
+                      name={`skills.${index}.category`}
+                      render={({ field }) => (
+                        <FormItem className="space-y-0">
+                          <FormLabel className={"text-base"}>
+                            Category
+                          </FormLabel>
+                          <Select
+                            defaultValue={field.value}
+                            disabled={field.disabled}
+                            onValueChange={field.onChange}
+                          >
+                            <FormControl>
+                              <SelectTrigger className="w-[280px]">
+                                <SelectValue placeholder="Category" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              {/* TODO: Use categories from database */}
+                              <SelectItem value="languages">
+                                Languages
+                              </SelectItem>
+                              <SelectItem value="artsAndHumanities">
+                                Arts & Humanities
+                              </SelectItem>
+                              <SelectItem value="businessAndMarketing">
+                                Business & Marketing
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
 
-        <Button variant="outline" type="submit">
-          {form.formState.isValid ? "Next" : "Skip"}
-        </Button>
-      </form>
+                    <FormField
+                      control={form.control}
+                      name={`skills.${index}.skill`}
+                      render={({ field }) => (
+                        <FormItem className="space-y-0">
+                          <FormLabel className={"text-base"}>Skill</FormLabel>
+                          <Select
+                            defaultValue={field.value}
+                            disabled={field.disabled}
+                            onValueChange={field.onChange}
+                          >
+                            <FormControl>
+                              <SelectTrigger className="w-[280px]">
+                                <SelectValue placeholder="Skill" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <SelectItem value="english">English</SelectItem>
+                              <SelectItem value="history">History</SelectItem>
+                              <SelectItem value="economics">
+                                Economics
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    {skillsFieldArray.fields.length > 1 && (
+                      <Button
+                        size={"icon"}
+                        variant={"destructive"}
+                        onClick={() => handleRemoveSkill(index)}
+                      >
+                        <CircleMinus />
+                      </Button>
+                    )}
+                  </fieldset>
+                ))}
+                <Button size={"sm"} type={"button"} onClick={handleAddSkill}>
+                  <CirclePlus />
+                  Add skill
+                </Button>
+              </div>
+            </TabsContent>
+          </Tabs>
+          <FormField
+            control={form.control}
+            name="profileDescription"
+            render={({ field }) => (
+              <FormItem className="space-y-0">
+                <FormLabel className={"text-base"}>
+                  Profile description
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little bit about yourself, this information will be public in your profile."
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            variant="outline"
+            type="submit"
+            disabled={!form.formState.isValid}
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
     </Form>
   );
 }
