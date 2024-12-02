11bf5cfeff04848dd878f0070f550df072febb7a
Veselin Ivanov <me@veselyn.com>
2024-12-02 16:51:32 +0100

    fix: skills form schema

--- app/signup/skills/schema.ts
+++ app/signup/skills/schema.ts
@@ -2,18 +2,34 @@ import { z } from "zod";
 
 export type SkillsFormValues = z.infer<typeof SkillsFormSchema>;
 
-export const SkillsFormSchema = z.object({
-  teachingSkills: z.array(
+const skillsSchema = z
+  .array(
     z.object({
-      category: z.string(),
-      skill: z.string(),
+      category: z.string().min(1),
+      skill: z.string().min(1),
     }),
-  ),
-  learningSkills: z.array(
-    z.object({
-      category: z.string(),
-      skill: z.string(),
-    }),
-  ),
-  profileDescription: z.string().min(1),
-});
+  )
+  .min(1);
+
+export const SkillsFormSchema = z
+  .object({
+    profileDescription: z.string().min(1),
+  })
+  .and(
+    z.union([
+      z.object({
+        teachingSkills: skillsSchema,
+        learningSkills: skillsSchema.length(0),
+      }),
+
+      z.object({
+        teachingSkills: skillsSchema.length(0),
+        learningSkills: skillsSchema,
+      }),
+
+      z.object({
+        teachingSkills: skillsSchema,
+        learningSkills: skillsSchema,
+      }),
+    ]),
+  );
