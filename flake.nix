aa395fcb0999882f2ce4dad69820251ecd556f4f
Veselin Ivanov <me@veselyn.com>
2024-12-23 15:06:22 +0200

    build(flake): init Veselin's env

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "chronocademy";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
+    treefmt-nix.url = "github:numtide/treefmt-nix/main";
+    devenv.url = "github:cachix/devenv/main";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} ({self, ...}: {
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+
+      perSystem = {
+        config,
+        pkgs,
+        self',
+        ...
+      }: let
+        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+
+          programs = {
+            alejandra.enable = true;
+            prettier.enable = true;
+          };
+        };
+      in {
+        devShells.default = inputs.devenv.lib.mkShell {
+          inherit inputs pkgs;
+
+          modules = [
+            {
+              languages = {
+                nix.enable = true;
+                javascript.enable = true;
+                javascript.npm.enable = true;
+              };
+
+              packages = [
+                pkgs.pgcli
+                self'.formatter
+              ];
+
+              pre-commit.hooks = {
+                deadnix.enable = true;
+                eslint.enable = true;
+                statix.enable = true;
+                treefmt.enable = true;
+                treefmt.package = self'.formatter;
+              };
+            }
+          ];
+        };
+
+        packages = {
+          devenv-test = self'.devShells.default.config.test;
+          devenv-up = self'.devShells.default.config.procfileScript;
+        };
+
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+      };
+    });
+}
