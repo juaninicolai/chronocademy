2942106155aca6baf4ca4bfae8bd14473807059b
Veselin Ivanov <me@veselyn.com>
2025-01-19 18:35:09 +0100

    feat: adjust footer with extra links

--- app/app/footer.tsx
+++ app/app/footer.tsx
@@ -1,16 +1,15 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Mail } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export function Footer() {
   return (
     <footer
       className={cn(
-        "h-16",
         "flex",
-        "items-center",
         "justify-between",
         "relative",
         "before:shadow-md",
@@ -18,9 +17,12 @@ export function Footer() {
         "before:absolute",
         "before:rotate-180",
         "before:pointer-events-none",
+        "pl-2",
+        "py-2",
       )}
     >
-      <div>
+      <div className="flex flex-col gap-1 items-start">
+        <p className="text-base">Quick links</p>
         <Button asChild variant="link">
           <Link href="mailto:info@chronocademy.com?subject=Bug report">
             Report a bug
@@ -31,9 +33,26 @@ export function Footer() {
             Request a feature
           </Link>
         </Button>
+        <Button asChild variant="link">
+          <Link href="/tos">Terms of Service</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/privacy">Privacy policy</Link>
+        </Button>
       </div>
 
-      <Image src={Logo} alt="chronocademy logo" className="h-full" />
+      <div>
+        <p className="text-base">Contact support</p>
+        <Button asChild variant="link">
+          <Link href="mailto:support@chronocademy.com">
+            <Mail /> support@chronocademy.com
+          </Link>
+        </Button>
+      </div>
+
+      <Link href="/" className="h-16">
+        <Image src={Logo} alt="chronocademy logo" className="h-full" />
+      </Link>
     </footer>
   );
 }
